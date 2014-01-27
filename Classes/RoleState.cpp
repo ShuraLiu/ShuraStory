6cce3152dac14f7a484de078817e9673e1ae2881
Shura.Liu <liuxiaoyi@verycd.com>
2014-01-27 17:39:16 +0800

    重新提交代码

--- Classes/RoleState.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  RoleState.cpp
-//  mygame
-//
-//  Created by 刘 潇逸 on 14-1-21.
-//
-//
-
-#include "RoleState.h"
--- Resources/mv_games.sh
+++ Resources/mv_games.sh
@@ -14,4 +14,20 @@ if ! mv game_1.pvr.ccz resources-iphone/ ; then
     echo "mv game_1.pvr.ccz error!"
 fi
 
+python remove_hd_suffix.py game_2-hd.plist -hd ''
+
+#game_2
+if ! mv game_2-hd.plist resources-iphonehd/game_2.plist ; then
+	echo "mv game_2-hd.plist error!"
+fi
+if ! mv game_2-hd.pvr.ccz resources-iphonehd/game_2.pvr.ccz ; then
+    echo "mv game_2-hd.pvr.ccz error!"
+fi
+if ! mv game_2.plist resources-iphone/ ; then
+	echo "mv game_2.plist error!"
+fi
+if ! mv game_2.pvr.ccz resources-iphone/ ; then
+    echo "mv game_2.pvr.ccz error!"
+fi
+
 echo "done!"
